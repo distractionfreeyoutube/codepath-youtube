724ab532f0a16f98daa27df90d436856552897c7
ChristianPinto-hub <72939362+ChristianPinto-hub@users.noreply.github.com>
2021-12-03 23:43:20 -0500

    Implements timer controller

--- DistractionFreeYoutube/DistractionFreeYoutube/TimerViewController.swift
+++ DistractionFreeYoutube/DistractionFreeYoutube/TimerViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
+    @IBOutlet weak var setTimerButton: UIButton!
+    @IBOutlet weak var timerTextField: UITextField!
+    var minutesRemaining: Int = 0
+    var secondsRemaining: Int = 0
+    @IBOutlet weak var timeRemainingTextField: UILabel!
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTimerButton.layer.cornerRadius = 10
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -25,5 +30,36 @@ class TimerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func setTimer(_ sender: Any) {
+        minutesRemaining = Int(timerTextField.text!)!
+        secondsRemaining = 0
+        timeRemainingTextField.text = "\(minutesRemaining):00"
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(step), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseTapped(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @objc func step() {
+        if (minutesRemaining == 0 && secondsRemaining == 0) {
+            timer.invalidate()
+            secondsRemaining = 0
+            timeRemainingTextField.text = "\(Int(timerTextField.text!)!):00"
+        } else if (secondsRemaining == 0) {
+            secondsRemaining = 59
+            minutesRemaining -= 1
+            timeRemainingTextField.text = "\(minutesRemaining):\(secondsRemaining)"
+        } else if (secondsRemaining <= 10){
+            secondsRemaining -= 1
+            timeRemainingTextField.text = "\(minutesRemaining):0\(secondsRemaining)"
+        } else {
+            secondsRemaining -= 1
+            timeRemainingTextField.text = "\(minutesRemaining):\(secondsRemaining)"
+        }
+    }
 }
