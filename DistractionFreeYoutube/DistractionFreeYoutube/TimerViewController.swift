63635e33e5c8d3be36be9406a8bd28972116f09b
ChristianPinto-hub <72939362+ChristianPinto-hub@users.noreply.github.com>
2021-12-16 16:34:23 -0500

    Implements alarm ring when timer finishes, vibrate option, and timer progress ring animation

--- DistractionFreeYoutube/DistractionFreeYoutube/TimerViewController.swift
+++ DistractionFreeYoutube/DistractionFreeYoutube/TimerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TimerViewController: UIViewController {
     
@@ -16,9 +17,43 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var timeRemainingTextField: UILabel!
     var timer: Timer!
     
+    var pause = false
+    var firstPlay = true
+    
+    @IBOutlet weak var ringerSwitch: UISwitch!
+    let systemSoundID: SystemSoundID = 1304
+    var soundEnabled = true
+    
+    let shape = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTimerButton.layer.cornerRadius = 10
+        
+        let circlePath = UIBezierPath(
+            arcCenter: CGPoint(
+                x: UIScreen.main.bounds.size.width*0.5,
+                y: UIScreen.main.bounds.size.height*0.36),
+            radius: 100,
+            startAngle: -(.pi / 2),
+            endAngle: .pi * 3 / 2,
+            clockwise: true)
+        
+        let trackShape = CAShapeLayer()
+        trackShape.path = circlePath.cgPath
+        trackShape.lineWidth = 15
+        trackShape.strokeColor = UIColor.lightGray.cgColor
+        trackShape.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(trackShape)
+        
+        shape.path = circlePath.cgPath
+        shape.lineWidth = 15
+        shape.strokeColor = UIColor(red: 250/255, green: 128/255, blue: 114/255, alpha: 1).cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        view.layer.addSublayer(shape)
+        
+        
     }
     
     /*
@@ -39,17 +74,40 @@ class TimerViewController: UIViewController {
     
     @IBAction func playTapped(_ sender: Any) {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(step), userInfo: nil, repeats: true)
+        
+        //if first play do the following, else should continue animation
+        if firstPlay {
+            firstPlay = false
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.toValue = 1
+            animation.duration = Double(timerTextField.text!)! * 60
+            animation.isRemovedOnCompletion = false
+            animation.fillMode = .forwards
+            shape.add(animation, forKey: "animation")
+        } else {
+            changeState()
+        }
     }
     
     @IBAction func pauseTapped(_ sender: Any) {
         timer.invalidate()
+        changeState()
     }
     
     @objc func step() {
         if (minutesRemaining == 0 && secondsRemaining == 0) {
             timer.invalidate()
             secondsRemaining = 0
-            timeRemainingTextField.text = "\(Int(timerTextField.text!)!):00"
+//            timeRemainingTextField.text = "\(Int(timerTextField.text!)!):00"
+            timeRemainingTextField.text = "\(minutesRemaining):00"
+            if soundEnabled {
+                for _ in 1...5 {
+                    AudioServicesPlaySystemSound(systemSoundID)
+                }
+            } else {
+                AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            }
+            firstPlay = true
         } else if (secondsRemaining == 0) {
             secondsRemaining = 59
             minutesRemaining -= 1
@@ -62,4 +120,38 @@ class TimerViewController: UIViewController {
             timeRemainingTextField.text = "\(minutesRemaining):\(secondsRemaining)"
         }
     }
+    
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        if sender.isOn {
+            soundEnabled = true
+        } else {
+            soundEnabled = false
+        }
+    }
+    
+    func pauseLayer(layer: CALayer) {
+        let pausedTime: CFTimeInterval = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.speed = 0.0
+        layer.timeOffset = pausedTime
+    }
+
+    func resumeLayer(layer: CALayer) {
+        let pausedTime: CFTimeInterval = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePause: CFTimeInterval = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        layer.beginTime = timeSincePause
+    }
+    
+    @IBAction func changeState() {
+        let layer = shape
+        pause = !pause
+        if pause {
+            pauseLayer(layer: layer)
+        } else {
+            resumeLayer(layer: layer)
+        }
+    }
+    
 }
